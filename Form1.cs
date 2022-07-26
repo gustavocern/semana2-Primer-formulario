07c08f642f4019c1cbb1ad8da842792ab2620b46
gustavocern <gustavoargueta363@gmail.com>
2022-07-25 21:09:50 -0500

    Ya ingrese el boton y lo label.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace semana2_Primer_formulario
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
